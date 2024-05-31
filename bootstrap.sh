8afe9068de38f846951dd9fe62915bdf2c18257b
Gerret Brandt <gerret.brandt@posteo.de>
2024-05-31 22:05:15 +0200

    print more empty lines and run playbook after brew update

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,33 +15,45 @@ dir=$(dirname -- "$( readlink -f -- "$0"; )")
 
 brew="$(command -v brew)"
 if [ -x "$brew" ]; then
+  echo ""
   echo "$me: Hombrew is already installed: $brew"
 
+  echo ""
   echo "$me: Update Hombrew ..."
   # /opt/homebrew/bin/brew update
   /usr/local/bin/brew update
 
+  echo ""
   echo "$me: Upgrade outdated casks and outdated, unpinned formulae ..."
   # /opt/homebrew/bin/brew upgrade
   /usr/local/bin/brew upgrade
+
+  echo ""
+  echo "$me: Run Ansible playbook ..."
+  /bin/bash -c "ansible-playbook -v $dir/playbook.yaml"
 else
+  echo ""
   echo "$me: Install Homebrew ..."
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   brew="$(command -v brew)"
   if [ -x "$brew" ]; then
+    echo ""
     echo "$me: Homebrew was successfully installed!"
 
+    echo ""
     echo "$me: Opt out of Hombrew analytics ..."
     # /opt/homebrew/bin/brew analytics off
     /usr/local/bin/brew analytics off
     # echo "$(/opt/homebrew/bin/brew analytics)"
     echo "$(/usr/local/bin/brew analytics)"
 
+    echo ""
     echo "$me: Install Ansible ..."
     # /opt/homebrew/bin/brew install ansible
     /usr/local/bin/brew install ansible
 
+    echo ""
     echo "$me: Run Ansible playbook ..."
     /bin/bash -c "ansible-playbook -v $dir/playbook.yaml"
 
