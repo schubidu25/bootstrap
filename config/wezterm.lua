709a34ac70c1c7603f5274edb41def0e4de47d54
Gerret Brandt <gerret.brandt@posteo.de>
2024-06-01 16:17:17 +0200

    wexterm config

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,15 +1,32 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local mux = wezterm.mux
+local act = wezterm.action
+
+-- wezterm.on('gui-startup', function()
+--   local tab, pane, window = mux.spawn_window({})
+--    window:gui_window():maximize()
+-- end)
 
 config.default_prog = { '/usr/local/bin/fish', '-l'}
+config.window_decorations = 'RESIZE'
+-- config.hide_tab_bar_if_only_one_tab = true
 
 -- config.color_scheme = 'AdventureTime'
 -- config.color_scheme = 'Batman'
 -- config.color_scheme = 'Aci'
--- config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 16
+
+config.inactive_pane_hsb = {
+    saturation = 0.8,
+    brightness = 0.7,
+}
+
+scrollback_lines = 4000
 
 -- config.window_background_image = 'wallpaper/darth_vader.png'
-config.window_background_image = 'wallpaper/darth_vader_03.jpeg'
+-- config.window_background_image = 'wallpaper/darth_vader_03.jpeg'
 -- config.window_background_image = '/Users/gerret/.config/wezterm/wallpaper/darth_vader_02.png'
 -- config.window_background_image = '/Users/gerret/.config/wezterm/wallpaper/kylo_ren.png'
 -- config.window_background_image = '/Users/gerret/.config/wezterm/wallpaper/darth_vader_03.jpeg'
@@ -25,6 +42,8 @@ config.window_background_image = 'wallpaper/darth_vader_03.jpeg'
 --     blend = 'Rgb',
 -- }
 -- config.window_background_opacity = 0.8
+
+
 
 
 return config
